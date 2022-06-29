66263a42c2b4947069538168654e5719341d4c9a
Nathan Workman <57638878+Nathan-Workman@users.noreply.github.com>
2022-06-29 00:08:01 -0400

    Add files via upload

---
+++ End.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider ProlougeEnd)
+    {
+        StartCoroutine(waitCoroutine());
+
+        IEnumerator waitCoroutine()
+        {
+            yield return new WaitForSeconds(120);
+
+
+            //if (other.tag == "End")
+                
+                Application.LoadLevel("Welcome");
+
+
+        }
+
+    }
+}
---
+++ TextScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class TextScreen : MonoBehaviour
+{
+
+    void Start()
+    
+    {
+        StartCoroutine(waitCoroutine());
+
+        IEnumerator waitCoroutine()
+        {
+            yield return new WaitForSeconds(20);
+
+
+            //if (other.tag == "End")
+
+            Application.LoadLevel("Showcase");
+
+
+        }
+
+    }
+}
